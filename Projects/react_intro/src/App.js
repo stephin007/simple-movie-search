d6f319dfecf667db55e6912d3259d3bc212562ad
Stephin Reji <stephinreji@gmail.com>
2023-04-22 18:24:01 +0530

    Added Loader file

--- Projects/react_intro/src/App.js
+++ Projects/react_intro/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import MovieCard from "./MovieCard";
+import LoadingSpinner from "./LoadingSpinner";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
@@ -9,45 +10,54 @@ const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=dd4e8195";
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     searchMovies("Batman");
   }, []);
 
   const searchMovies = async (title) => {
+    setIsLoading(true);
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
     setMovies(data.Search);
+    setIsLoading(false);
   };
 
   return (
-    <div className="app">
+    <div className='app'>
       <h1>MovieLand</h1>
 
-      <div className="search">
+      <div className='search'>
         <input
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Search for movies"
+          placeholder='Search for movies'
         />
         <img
           src={SearchIcon}
-          alt="search"
+          alt='search'
           onClick={() => searchMovies(searchTerm)}
         />
       </div>
 
-      {movies?.length > 0 ? (
-        <div className="container">
-          {movies.map((movie) => (
-            <MovieCard movie={movie} />
-          ))}
-        </div>
+      {!isLoading ? (
+        <>
+          {movies?.length > 0 ? (
+            <div className='container'>
+              {movies.map((movie) => (
+                <MovieCard movie={movie} />
+              ))}
+            </div>
+          ) : (
+            <div className='empty'>
+              <h2>No movies found</h2>
+            </div>
+          )}
+        </>
       ) : (
-        <div className="empty">
-          <h2>No movies found</h2>
-        </div>
+        <LoadingSpinner />
       )}
     </div>
   );
